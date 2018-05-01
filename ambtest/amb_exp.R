830b5c6d8b41464352e4322f9c0bc730ff584cc6
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2018-05-01 00:25:43 -0400

    datadr and rhipe code clean up. correlation based subset selection after recombination.

--- ambtest/amb_exp.R
+++ ambtest/amb_exp.R
@@ -1,36 +1,30 @@
-
 library(RSEIS)
 library(RPMG)
 library(Rwave)
-options(continue = " ")
-#mymergeddata = multmerge("/home/didi/ambtest")
 library(datadr)
 library(signal)
 library(pracma)
-
 
 fnames = list.files(path='./data2',
                     pattern=NULL, full.names=TRUE)
 num_station = length(fnames)
 
-beds = list()
+stations = list()
 dt = 0.004
 fs=1/dt;
-
-
 n = 8000
+
 for (i in 1:num_station) {
   fn = fnames[i]
-  bed =  read1sac(fn , Iendian = 1 , HEADONLY=FALSE, BIGLONG=FALSE)
-  bed = data.frame(HEAD.kstnm = bed$HEAD$kstnm, amp = bed$amp)
-  N <- length(bed$amp)
-  NN <- n * as.integer(N/n)
-  NN <- NN - 1
-  
-  bed <- bed[1:NN,]
-  #bed$HEAD.time = (1:length(bed$HEAD.npts)-1)*dt
-  bed <- bed[,c('HEAD.kstnm', 'amp')]
-  beds[[i]] <- bed
+  station =  read1sac(fn , Iendian = 1 , HEADONLY=FALSE, BIGLONG=FALSE)
+  station = data.frame(station = station$HEAD$kstnm, amp = station$amp)
+  N_orig <- length(station$amp)
+  nn <- as.integer(N_orig/n)
+  station <- station[-nrow(station),]
+  station <- station[-nrow(station),]
+  #station$HEAD.time = (1:length(station$HEAD.npts)-1)*dt
+  station <- station[,c('station', 'amp')]
+  stations[[i]] <- station
 } 
 
 
@@ -39,7 +33,7 @@ dataKV <- list()
 for (i in 1:num_station) {
   st_name <- paste('station', i, sep="")
   st_id <- as.numeric(paste('100', i, sep=""))
-  dataKV[[i]] <- kvPair(st_name, subset(beds[[i]], HEAD.kstnm == st_id))
+  dataKV[[i]] <- kvPair(st_name, subset(stations[[i]], station == st_id))
 }
 
 
@@ -47,12 +41,10 @@ for (i in 1:num_station) {
 datakvDdf <- ddf(dataKV)
 
 byamp <- divide(datakvDdf, 
-                by ="HEAD.kstnm",
+                by ="station",
                 spill = n,
                 #output = hdfsConn(dirs, autoYes=TRUE),
                 update=TRUE)
-
-
 
 b1= butter(2, c(0.01/(fs/2), 3/(fs/2)))
 
@@ -72,6 +64,7 @@ signbit <- function(data){
 time=(0:(n-1))*dt
 l = n/2
 proccc <- addTransform(byamp, function(v) {
+  v$amp[is.na(v$amp)] <- 0
   a = v$amp -  mean(v$amp)
   a = detrend(a)
   a = filtfilt(b1, a, type="pass")
@@ -93,48 +86,137 @@ proccc <- addTransform(byamp, function(v) {
   # spectral whitening can be done dividing the power spectrum of autocorrelated data to smoothed data . add a little damping to the denominator
   wh_sta_22 = a_22_spec$spec / (s_22_spec$spec + 0.00001)
   wh_sta_22_time = abs(ifft((wh_sta_22)))
-  b2= butter(2, c(6/(fs/2), 12/(fs/2)))
+  b2= butter(2, c(10/(fs/2), 20/(fs/2)))
   result_station_22 <- filtfilt(b2, wh_sta_22_time, type="pass")
 })
 last = recombine(proccc, combRbind)
 
+cor_threshhold <- 0.90
+percent_subset_match <- 0.50
+
 station = list()
+station_m = list()
+station_corm = list()
+selected_subsets_per_station = list()
+n_selected_subsets_per_station = c()
 
-for (i in 1:num_station) {
-  st_number = as.numeric(paste("100", i, sep=""))
-  station[[i]] = subset(last, last$HEAD.kstnm == st_number)
+for (k in 1:num_station) {
+  st_number = as.numeric(paste("100", k, sep=""))
+  station[[k]] = subset(last, last$station == st_number)
+  
+  m = n/4
+  v = station[[k]]$val
+  station_m[[k]] = matrix(v, nrow=m, byrow=FALSE)
+  station_m[[k]] = station_m[[k]][1:(m/2 - 1), ]
+  station_corm[[k]] = cor(station_m[[k]])
+  nn <- nrow(station_corm[[k]]) # number of subsets in each station
+  #print(station_corm[[i]])
+  # rgb.palette <- colorRampPalette(c("blue", "yellow"), space = "rgb")
+  # # levelplot(station_corm[[i]], 
+  # #           main=paste("Station ", i, sep=""), 
+  # #           xlab="", ylab="", 
+  # #           col.regions=rgb.palette(120), 
+  # #           cuts=100, at=seq(0,1,0.01))
+  
+  selected_stations = list() # selected subsets for each station
+  n_selected_stations = c()
+  for(i in 1:nn) {
+    selecteds = c()
+    for(j in 1:nn) {
+      if (i==j)
+        next 
+      if(station_corm[[k]][i,j] > cor_threshhold) 
+        selecteds = c(selecteds, j)
+    }
+    selected_stations[[i]] = selecteds #selected for station i
+    n_selected_stations = c(n_selected_stations, length(selecteds))
+  }
+  
+  selected_subsets = rep(0, nn)
+  ns <- length(n_selected_stations)
+  for (i in 1:ns) {
+    c = n_selected_stations[i]
+    if (c > as.integer(percent_subset_match * nn)) {
+      #print(c)
+      selected_subsets[[i]]  = 1
+    }
+  }
+  selected_subsets_per_station[[k]] = selected_subsets
+  SS <- selected_subsets_per_station[[k]]
+  nSS <- length(SS[SS == 1])
+  n_selected_subsets_per_station = c(n_selected_subsets_per_station, nSS)
 }
 
-
-for (i in 1:num_station) {
-  t = paste("Amplitude of noise from station", i)
-  plot(station[[i]]$val[1:l], type='l', col=i, ylab='Amp', main=t)
-}
-
-
+# 
+# 
+# for (i in 1:num_station) {
+#   t = paste("Amplitude of noise from station", i)
+#   plot(station[[i]]$val[1:l], type='l', col=i, ylab='Amp', main=t)
+# }
+# 
 
 m = n/4
+mm = (m/2)-1
 st_sum = list()  
-j = 1
-for(st in station) {
+for(j in 1:num_station) {
+  st = station[[j]]
   cur_st_sum = rep(0, m)
-  i = 1
-  for (stv in st$val) {
-    idx = i %% m + 1
-    cur_st_sum[[idx]] = cur_st_sum[[idx]] + stv
-    #print(idx)
-    #print("")
-    i = i+1
+  nV <- length(st$val)
+  for (i in 1:nV) {
+    stv <- st$val[i]
+    #print(i)
+    subset_idx = as.integer((i-1)/m) + 1
+    #print(subset_idx)
+    if (selected_subsets_per_station[[j]][subset_idx] == 1) {
+      idx = i %% m + 1
+      cur_st_sum[[idx]] = cur_st_sum[[idx]] + stv
+    }
   }
   st_sum[[j]] = cur_st_sum
-  j = j+1
 }
 
-par(mar=c(4,4,4,4))
+#pdf()
+#par(mar=c(4,4,4,4))
 #par(mfrow=c(num_stations,1))
 time = (0:(n/4 - 1)) * dt
 for (i in 1:num_station) {
   t = paste("Summed Amplitude of noise from station", i)
-  plot(rev(st_sum[[i]]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
+  plot(rev(st_sum[[i]][1:mm]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
   abline(v=0, lty=2)
 }
+#dev.off()
+
+
+########
+# station = list()
+# 
+# for (i in 1:num_station) {
+#   st_number = as.numeric(paste("100", i, sep=""))
+#   station[[i]] = subset(last, last$station == st_number)
+# }
+# 
+# m = n/4
+# st_sum = list()  
+# j = 1
+# for(st in station) {
+#   cur_st_sum = rep(0, m)
+#   i = 1
+#   for (stv in st$val) {
+#     idx = i %% m + 1
+#     cur_st_sum[[idx]] = cur_st_sum[[idx]] + stv
+#     #print(idx)
+#     #print("")
+#     i = i+1
+#   }
+#   st_sum[[j]] = cur_st_sum
+#   j = j+1
+# }
+# 
+# #par(mar=c(4,4,4,4))
+# #par(mfrow=c(num_stations,1))
+# time = (0:(n/4 - 1)) * dt
+# for (i in 1:num_station) {
+#   t = paste("Summed Amplitude of noise from station", i)
+#   plot(rev(st_sum[[i]]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
+#   abline(v=0, lty=2)
+# }
--- ambtest/amb_exp_rhipe.R
+++ ambtest/amb_exp_rhipe.R
@@ -16,20 +16,17 @@ library(Rwave)
 library(datadr)
 library(signal)
 library(pracma)
+library(datadr)
 
 # plot in a pdf file
 # set the margin of the plot
 par(mar=c(1,1,1,1))
 
-user <- 'azehady/'
-data_file_path = './data5hours/'
-project_dir_name = 'interferometry_5hours'
-
-#data_file_path = './data2/'
-#project_dir_name = 'data2'
+data_file_name = './data2/'
+dir <- 'data2' # project directory
 
 # get the file names from the directory in the server
-fnames = list.files(path=data_file_path,
+fnames = list.files(path=data_file_name,
                     pattern=NULL, full.names=TRUE)
 
 # get the number of the stations
@@ -38,20 +35,15 @@ num_station = length(fnames)
 Amp <- list()
 
 # create the user directory and move there
+user <- 'eergun/'
 user_dir <- paste('/user/', user, sep="")
 hdfs.setwd(user_dir)
 
-# remove tmp files
-tmp_dir <- paste(user_dir, 'tmp/*', sep="")
-rhdel(tmp_dir)
-
 # Create the project directory 
-project_dir <- paste(user_dir, project_dir_name, sep="")
-
-if (rhexists(project_dir))
-  rhdel(project_dir)
-
-rhmkdir(project_dir)
+project_dir <- paste(user_dir, dir, sep="")
+rhdel(project_dir)
+if (!rhexists(project_dir))
+  rhmkdir(project_dir)
 
 # change to the project directory
 hdfs.setwd(project_dir)
@@ -78,38 +70,31 @@ for (i in 1:num_station) {
   st_num <- as.character(paste('100', i, sep="")) 
   station <- read1sac(fn , Iendian = 1 , HEADONLY=FALSE, BIGLONG=FALSE)
   
-  N <- length(station$amp)
-  NN <- n * as.integer(N/n)
-  NN <- NN - 1
-  #print(fn)
-  #print(NN)
+  N_orig <- length(station$amp)
   
-  station$station <- rep(st_num, N)
+  nn <- as.integer(N_orig/n)
+  
+  N <- N_orig - 2 #nn * n - 1 
+  #N_del <- N_orig - N 
+  
+  station$station <- rep(st_num, N_orig)
   stationDF <- data.frame(station=station['station'], amp=station['amp'])
-  
-  stationDF <- stationDF[1:NN, ]
+  stationDF <- stationDF[1:N, ]
   
   start_idx <- 1 
   
-  k <- 20
-  nn <- (NN + 1) / n
-  nnn <- as.integer(nn / k)
-  
-  if (nnn != 0)  
-    num_rows <- nnn * n
-  else
-    num_rows <- n
-  
+  k <- 10
+  num_rows <- 2*n
   end_idx <- as.integer(num_rows)
  
   j <- 1 
   breakout = FALSE
   while(1) {
-    #print(start_idx)
-    #print(end_idx)
-    #print("")
-    if (end_idx > NN) {
-      end_idx = NN
+    print(start_idx)
+    print(end_idx)
+    print("")
+    if (end_idx > N) {
+      end_idx = N
       breakout = TRUE
     }
     jN <- length(start_idx:end_idx)
@@ -129,7 +114,7 @@ for (i in 1:num_station) {
   }
 }  
 
-hdfs_dir <- paste(user_dir, project_dir_name,"/", data_dir, sep="")
+hdfs_dir <- paste(user_dir, dir,"/", data_dir, sep="")
 seismHDFSconn <- hdfsConn(hdfs_dir, autoYes = TRUE)
 
 datakvDdf <- ddf(seismHDFSconn) 
@@ -162,6 +147,7 @@ time=(0:(n-1))*dt
 l = n/2
 proccc <- addTransform(byamp, function(v) {
   #print(length(v$amp))
+  v$amp[is.na(v$amp)] <- 0
   a = v$amp -  mean(v$amp)
   a = detrend(a)
   a = filtfilt(b1, a, type="pass")
@@ -191,86 +177,181 @@ last = recombine(proccc, combRbind)
 
 hdfs.setwd(project_dir)
 
-# Now each station data will be saved as chunk
-m <- n/4
-for (i in 1:num_station) {
-  fn = fnames[i]
-  st_num <- as.character(paste('100', i, sep="")) 
-  station = subset(last, last$station == st_num)
+cor_threshhold <- 0.90
+percent_subset_match <- 0.50
+
+station = list()
+station_m = list()
+station_corm = list()
+selected_subsets_per_station = list()
+n_selected_subsets_per_station = c()
+
+for (k in 1:num_station) {
+  st_number = as.numeric(paste("100", k, sep=""))
+  station[[k]] = subset(last, last$station == st_number)
   
-  NN <- length(station$val)
-  stationDF <- data.frame(station = station$station, amp = station$val)
- 
-  output_dir <- paste('Station_', i, '_Output', sep="")
-  if (rhexists(output_dir))
-    rhdel(output_dir)  
-  rhmkdir(output_dir)
+  m = n/4
+  v = station[[k]]$val
+  station_m[[k]] = matrix(v, nrow=m, byrow=FALSE)
+  station_m[[k]] = station_m[[k]][1:(m/2 - 1), ]
+  station_corm[[k]] = cor(station_m[[k]])
+  #print(station_corm[[i]])
+  # rgb.palette <- colorRampPalette(c("blue", "yellow"), space = "rgb")
+  # # levelplot(station_corm[[i]], 
+  # #           main=paste("Station ", i, sep=""), 
+  # #           xlab="", ylab="", 
+  # #           col.regions=rgb.palette(120), 
+  # #           cuts=100, at=seq(0,1,0.01))
   
-  hdfs.setwd(output_dir)
-  
-  fD <- as.integer(NN / m)
-  
-  start_idx <- 1 
-  end_idx <- as.integer(m)
-  for (j in 1:fD) {
-    if (j == fD)
-      end_idx <- NN
-    
-    jN <- length(start_idx:end_idx)
-    stationKV <- list(list(key=st_num, value=stationDF[start_idx:end_idx, ])) 
-    
-    stationDataFile <- paste(j, "_station_output_", "Data", sep="")
-    print(paste("Writing ", stationDataFile, " output file in HDFS."))
-    rhwrite(stationKV, file=stationDataFile)
-    
-    start_idx <- end_idx + 1 
-    end_idx <- start_idx + as.integer(m) - 1
-    
+  selected_stations = list()
+  n_selected_stations = c()
+  for(i in 1:nn) {
+    selecteds = list()
+    for(j in 1:nn) {
+      if (i==j)
+        next 
+      if(station_corm[[k]][i,j] > cor_threshhold) 
+        selecteds = c(selecteds, j)
+    }
+    selected_stations[[i]] = selecteds
+    n_selected_stations = c(n_selected_stations, length(selecteds))
   }
   
-  hdfs.setwd(project_dir)
-}  
-
-st_sum = list()
-
-for (i in 1:num_station) { 
-  print(paste("Processing station output", i))
-  tryCatch({
-    output_dir <- paste(user_dir, project_dir_name, "/", 'Station_', i, '_Output', sep="")
-    #print(rhls(output_dir))
-    outHDFSconn <- hdfsConn(output_dir, autoYes = TRUE)
-    outputkvDdf <- ddf(outHDFSconn)
-    outputkvDdf <- updateAttributes(outputkvDdf)
-    
-    byStation <- divide(outputkvDdf,
-                        by = 'station',
-                        spill = m
-                       )
-    byStationModified <- ddf(byStation[1:fD])
-    sumReduce <- addTransform(byStationModified, function(v) {
-          v$amp 
-    })
-    
-    st_sum[[i]] = recombine(sumReduce, combMean)
-  },
-  error = function(err){
-     print(err)
-  })
+  selected_subsets = rep(0, nn)
+  ns <- length(n_selected_stations)
+  for (i in 1:ns) {
+    c = n_selected_stations[i]
+    if (c > as.integer(percent_subset_match * nn)) {
+      #print(c)
+      selected_subsets[[i]]  = 1
+    }
+  }
+  selected_subsets_per_station[[k]] = selected_subsets
+  SS <- selected_subsets_per_station[[k]]
+  nSS <- length(SS[SS == 1])
+  n_selected_subsets_per_station = c(n_selected_subsets_per_station, nSS)
 }
-hdfs.setwd(project_dir)
+
+# 
+# 
+# for (i in 1:num_station) {
+#   t = paste("Amplitude of noise from station", i)
+#   plot(station[[i]]$val[1:l], type='l', col=i, ylab='Amp', main=t)
+# }
+# 
 
 
-for (i in 1:num_station) {
-  st_sum[[i]] <- st_sum[[i]] * fD
+m = n/4
+mm = (m/2)-1
+st_sum = list()  
+for(j in 1:num_station) {
+  st = station[[j]]
+  cur_st_sum = rep(0, m)
+  nV <- length(st$val)
+  for (i in 1:nV) {
+    stv <- st$val[i]
+    #print(i)
+    subset_idx = as.integer((i-1)/m) + 1
+    #print(subset_idx)
+    if (selected_subsets_per_station[[j]][subset_idx] == 1) {
+      idx = i %% m + 1
+      cur_st_sum[[idx]] = cur_st_sum[[idx]] + stv
+    }
+  }
+  st_sum[[j]] = cur_st_sum
 }
 
 pdf()
-par(mar=c(4,4,4,4))
+#par(mar=c(4,4,4,4))
 #par(mfrow=c(num_stations,1))
 time = (0:(n/4 - 1)) * dt
 for (i in 1:num_station) {
   t = paste("Summed Amplitude of noise from station", i)
-  plot(rev(st_sum[[i]]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
+  plot(rev(st_sum[[i]][1:mm]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
   abline(v=0, lty=2)
 }
 dev.off()
+
+# # Now each station data will be saved as chunk
+# m <- n/4
+# for (i in 1:num_station) {
+#   fn = fnames[i]
+#   st_num <- as.character(paste('100', i, sep="")) 
+#   station = subset(last, last$station == st_num)
+#   
+#   NN <- length(station$val)
+#   stationDF <- data.frame(station = station$station, amp = station$val)
+#  
+#   output_dir <- paste('Station_', i, '_Output', sep="")
+#   if (rhexists(output_dir))
+#     rhdel(output_dir)  
+#   rhmkdir(output_dir)
+#   
+#   hdfs.setwd(output_dir)
+#   
+#   fD <- as.integer(NN / m)
+#   
+#   start_idx <- 1 
+#   end_idx <- as.integer(m)
+#   for (j in 1:fD) {
+#     if (j == fD)
+#       end_idx <- NN
+#     
+#     jN <- length(start_idx:end_idx)
+#     stationKV <- list(list(key=st_num, value=stationDF[start_idx:end_idx, ])) 
+#     
+#     stationDataFile <- paste(j, "_station_output_", "Data", sep="")
+#     print(paste("Writing ", stationDataFile, " output file in HDFS."))
+#     rhwrite(stationKV, file=stationDataFile)
+#     
+#     start_idx <- end_idx + 1 
+#     end_idx <- start_idx + as.integer(m) - 1
+#     
+#   }
+#   
+#   hdfs.setwd(project_dir)
+# }  
+# 
+# st_sum = list()
+# 
+# for (i in 1:num_station) { 
+#   print(paste("Processing station output", i))
+#   tryCatch({
+#     output_dir <- paste(user_dir, dir, "/", 'Station_', i, '_Output', sep="")
+#     #print(rhls(output_dir))
+#     outHDFSconn <- hdfsConn(output_dir, autoYes = TRUE)
+#     outputkvDdf <- ddf(outHDFSconn)
+#     outputkvDdf <- updateAttributes(outputkvDdf)
+#     
+#     byStation <- divide(outputkvDdf,
+#                         by = 'station',
+#                         spill = m
+#                        )
+#     byStationModified <- ddf(byStation[1:fD])
+#     sumReduce <- addTransform(byStationModified, function(v) {
+#           v$amp 
+#     })
+#     
+#     st_sum[[i]] = recombine(sumReduce, combMean)
+#   },
+#   error = function(err){
+#      print(err)
+#   })
+# }
+# hdfs.setwd(project_dir)
+# 
+# 
+# for (i in 1:num_station) {
+#   st_sum[[i]] <- st_sum[[i]] * fD
+# }
+# 
+# pdf()
+# par(mar=c(4,4,4,4))
+# #par(mfrow=c(num_stations,1))
+# time = (0:(n/4 - 1)) * dt
+# for (i in 1:num_station) {
+#   t = paste("Summed Amplitude of noise from station", i)
+#   plot(rev(st_sum[[i]]), time, type='l', col=i, ylab='Time(s)', xlab='Amp', main=t, xlim=c(-max(st_sum[[i]]), max(st_sum[[i]])))
+#   abline(v=0, lty=2)
+# }
+# dev.off()
